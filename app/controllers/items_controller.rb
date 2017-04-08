f249c56c518b1524acdff8285cf7c1a0466f1b57
Jorge <jjoorrssxp@outlook.com>
2017-04-08 18:40:50 -0500

    Navigarion added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :find_item, except: [:index, :new, :create]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -14,12 +16,33 @@ class ItemsController < ApplicationController
       flash[:notice] = "Tarea creada"
       redirect_to root_path
     else
+      flash[:alert] = "La tarea no pudo ser creada"
       render 'new'
     end
+  end
+
+  def update
+    if @item.update(item_params)
+      flash[:notice] = "Tarea actualizada"
+      redirect_to @item
+    else
+      flash[:alert] = "La tarea no pudo ser actualizada"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    flash[:notice] = "Tarea eliminada"
+    redirect_to root_path
   end
 
   private
     def item_params
       params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
     end
 end
